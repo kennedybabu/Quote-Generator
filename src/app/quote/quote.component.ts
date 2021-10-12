458e5625d2af40614c96de195f6abb8798bb0fd0
kennedybabu <kennedynjogu8@gmail.com>
2021-10-12 14:21:18 +0300

    final commit

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -14,7 +14,8 @@ export class QuoteComponent implements OnInit {
   quotes: Quote[] = [
     new Quote(1, 'Carl Sagan', 'Extinction is the rule. Survival is the exception', 'bananaGuy', new Date(2021, 3, 14)),
     new Quote(2, 'George Carlin', 'If its true that our species is alone in the universe, then Id have to say the universe aimed rather low and settled for very little.', 'Charlie Brown', new Date(2020, 10, 10)),
-    new Quote(3, 'Dylan Thomas', 'Do not go gentle into that good night, Old age should burn and rave at close of day; Rage, rage against the dying of the light.', 'bigFoot', new Date),
+    new Quote(3, 'Dylan Thomas', 'Do not go gentle into that good night, Old age should burn and rave at close of day; Rage, rage against the dying of the light.', 'bigFoot', new Date()),
+    new Quote(4, 'Miriam Makeba', 'Age is getting to know all the ways the world turns, so that if you cannot turn the world the way you want, you can at least get out of the way so you wont get run over.', 'sathAfrikkan', new Date(2001, 2, 10))
  ]
 
 
